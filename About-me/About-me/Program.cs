b4c7be9a72fd4101ec7bc45b0add8b4a9c54698d
eDevolper <ebrahimsalehabdulraheem.alamodi@st.uskudar.edu.tr>
2022-07-18 22:05:25 +0300

    add about me

---
+++ About-me/About-me/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace About_me
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello There");
+            Console.WriteLine("My name is Ahmed");
+            Console.ReadLine();
+        }
+    }
+}
